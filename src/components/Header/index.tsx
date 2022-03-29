fae97af9f410c51ef78480178c86cac2bfef54d1
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-03-29 19:00:26 -0300

    responsividade Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../context/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationNav } from "./NotificationNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+    const { onOpen } = useSidebarDrawer();
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -21,8 +25,22 @@ export function Header() {
             px="6"
             align="center"
         >
+
+            {!isWideVersion && (
+                <IconButton
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    aria-label="Open navigation"
+                    mr="2"
+
+                >
+
+                </IconButton>
+            )}
             <Logo />
-            { isWideVersion && <SearchBox />}
+            {isWideVersion && <SearchBox />}
             <Flex
                 align="center"
                 ml="auto"
