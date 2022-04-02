15e8b76068b8291e1c8b54b7cadada0823dc15ac
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-04-02 11:51:27 -0300

    responsividade CreateUser

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,13 +9,13 @@ export default function CreateUser() {
             <Header />
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                <Box flex="1" borderRadius={8} bg="gray.800" p={["6","8"]}>
                     <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
                     <Divider my="6" borderColor="gray.700" />
                     <VStack spacing="8">
                         <SimpleGrid
                             minChildWidth="240px"
-                            spacing="8"
+                            spacing={["6","8"]}
                             w="100%"
                         >
                             <Input name="name" label="Nome completo" />
@@ -23,7 +23,7 @@ export default function CreateUser() {
                         </SimpleGrid>
                         <SimpleGrid
                             minChildWidth="240px"
-                            spacing="8"
+                            spacing={["6","8"]}
                             w="100%"
                         >
                             <Input name="password" type="password" label="Senha" />
